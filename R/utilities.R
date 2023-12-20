80828806eda0c46f10a08c4f6214183aa2d4f37f
Marius 't Hart <thartbm@gmail.com>
2023-12-20 09:08:55 -0500

    add functions for scatters and densities, change central tendency across participants to be the mean (within is median), remove last trial of rotation from washout data

--- R/utilities.R
+++ R/utilities.R
@@ -25,14 +25,29 @@ groupInfo <- function() {
   
 }
 
+expConditions <- function(exp) {
+  
+  if (exp == 1) { conditions <- c('15deg_distance', '30deg_distance', '45deg_distance', '60deg_distance') }
+  if (exp == 2) { conditions <- c('control','cursorjump','terminal') }
+  if (exp == 3) { conditions <- c('control','aiming') }
+  if (exp == 4) { conditions <- c('control','terminal','delay-trial', 'delay-FB')}
+  
+  return(conditions)
+  
+}
+
 # plotting -----
 
-addLearningCurves <- function(type, conditions=NULL, exp=NULL, phases=c('baseline','rotation','washout')) {
+addLearningCurves <- function(type, conditions=NULL, exp=NULL, phases=c('baseline','rotation','washout'),FUN=mean) {
   
-  if (type %notin% c('reaches','nocursors')) {
+  if (type %notin% c('reaches','nocursors','aiming')) {
     return()
   }
-
+  
+  depvar <- list('aiming'='aimingdeviation_deg',
+                 'reaches'='reachdeviation_deg',
+                 'nocursors'='reachdeviation_deg')[[type]]
+  
   info <- groupInfo()
   
   if (is.null(conditions)) {
@@ -52,17 +67,24 @@ addLearningCurves <- function(type, conditions=NULL, exp=NULL, phases=c('baselin
     
     idx <- which(info$condition == condition)
     exp <- info$exp[idx]
-    color[[condition]] <- info$color[idx]
+    if (type == 'aiming') {
+      color[[condition]] <- 'magenta'
+    } else {
+      color[[condition]] <- info$color[idx]
+    }
     label[[condition]] <- info$label[idx]
     
     df <- read.csv(sprintf('data/exp%d/%s_%s.csv', exp, condition, type))
     
-    df$reachdeviation_deg <- -1 * df$reachdeviation_deg
-    df <- removeOutliers(df)
-    df <- baseline(df)
+    df <- removeOutliers(df, depvar=depvar)
+    df <- baseline(df, depvar=depvar)
     
-    avg[[condition]] <- aggregate(reachdeviation_deg ~ trialno, data=df, FUN=median, na.rm=TRUE)
-    CIs[[condition]] <- aggregate(reachdeviation_deg ~ trialno, data=df, FUN=function(d) Reach::getConfidenceInterval(d, method='b', FUN=median))
+    df$depvar <- df[,depvar]
+    
+    df$depvar <- -1 * df$depvar
+    
+    avg[[condition]] <- aggregate(depvar ~ trialno, data=df, FUN=FUN, na.rm=TRUE)
+    CIs[[condition]] <- aggregate(depvar ~ trialno, data=df, FUN=function(d) Reach::getConfidenceInterval(d, method='b', FUN=FUN))
     
   }
   
@@ -72,7 +94,7 @@ addLearningCurves <- function(type, conditions=NULL, exp=NULL, phases=c('baselin
     CI <- CIs[[condition]]
     polygon(
       x = c(CI$trialno, rev(CI$trialno)),
-      y = c(CI$reachdeviation_deg[,1], rev(CI$reachdeviation_deg[,2])),
+      y = c(CI$depvar[,1], rev(CI$depvar[,2])),
       border=NA,
       col=cc
     )
@@ -82,7 +104,7 @@ addLearningCurves <- function(type, conditions=NULL, exp=NULL, phases=c('baselin
   for (condition in conditions) {
     cc <- color[[condition]]
     central <- avg[[condition]]
-    lines(central$trialno, central$reachdeviation_deg, col=cc)
+    lines(central$trialno, central$depvar, col=cc)
   }
   
   # make a legend
@@ -109,6 +131,84 @@ addAimingTrials <- function(mrot) {
          x1=trials,y1=rep(-1,length(trials)),
          length=0.01,
          col='black')
+  
+}
+
+addDensities <- function(conditions, type, flipXY, viewscale) {
+  
+  info <- groupInfo()
+  
+  for (condition in conditions) {
+    
+    plotdf <- getImpExpEst(condition=condition, type=type)
+    
+    dens <- density(x=plotdf$depvar,
+                    bw=5,
+                    from=-15,
+                    to=75,
+                    n=1001)
+    
+    if (flipXY) {
+      Y <- dens$x
+      X <- 1-(dens$y/max(dens$y))
+      # X <- 1-(dens$y * 14)
+    } else {
+      X <- dens$x
+      Y <- 1-(dens$y/max(dens$y))
+      # Y <- 1-(dens$y * 14)
+    }
+    
+    color <- info$color[which(info$condition == condition)]
+    
+    lines(x=X,
+          y=Y,
+          col=color)
+    
+  }
+  
+}
+
+addImpExpScatters <- function(conditions) {
+  
+  info <- groupInfo()
+  
+  for (condition in conditions) {
+    
+    # prep data:
+    impdf <- getImpExpEst(condition=condition, type='nocursors')
+    impdf$implicit <- impdf$depvar
+    impdf <- impdf[,c('participant','implicit')]
+    expdf <- getImpExpEst(condition=condition, type='aiming')
+    expdf$explicit <- expdf$depvar
+    expdf <- expdf[,c('participant','explicit')]
+    df <- merge(impdf, expdf, by='participant')
+    
+    # prep plotting:
+    color  <- info$color[which(info$condition == condition)]
+    acolor <- Reach::colorAlpha(col=color, alpha=44)
+    
+    # actual scatter plot:
+    points(df$explicit, df$implicit, col=acolor)
+    
+    # lm with 95% CI:
+    impl <- df$implicit
+    expl <- df$explicit
+    e2i <- lm(impl ~ expl)
+    
+    at <- range(df$explicit)
+    
+    coef <- e2i$coefficients
+    lines(at, coef[1]+(at*coef[2]), col=color)
+    
+    ci <- predict( e2i,
+                   newdata=data.frame(expl=seq(at[1],at[2],length.out=40)),
+                   interval = "confidence")
+    
+    X <- c(seq(at[1],at[2],length.out=40),rev(seq(at[1],at[2],length.out=40)))
+    Y <- c(ci[,'lwr'],rev(ci[,'upr']))
+    polygon(x=X,y=Y,col=acolor,border=NA)
+    
+  }
   
 }
 
@@ -155,8 +255,135 @@ getWashout <- function(df, asymptote=8, depvar='reachdeviation_deg') {
     # print(median(adf$tempvar[which(adf$participant == participant)], na.rm=TRUE))
   }
   
-  df <- df[which(df$trialno > (washoutStart-2)),]
+  # df <- df[which(df$trialno > (washoutStart-2)),]
+  df <- df[which(df$trialno >= washoutStart),]
   df$phase <- 'washout'
   return(df)
+  
+}
+
+
+# data ------
+
+getImpExpEst <- function(condition,type) {
+  
+  info <- groupInfo()
+  aiming_trials <- c( 77, 81, 85, 89, 93, 97, 101, 105 )
+  
+  exp <- info$exp[which(info$condition == condition)]
+  
+  df <- read.csv(file=sprintf('data/exp%d/%s_%s.csv',exp,condition,type), stringsAsFactors = FALSE)
+  
+  if (type == 'nocursors') {
+    df$depvar <- df$reachdeviation_deg
+    use_trials <- c(aiming_trials, aiming_trials-1)
+  }
+  if (type == 'aiming') {
+    df$depvar <- df$aimingdeviation_deg
+    use_trials <- aiming_trials
+  }
+  
+  
+  df[which(df$trialno %in% use_trials),]
+  df$depvar <- -1 * df$depvar
+  
+  newdf <- aggregate(depvar ~ participant, data=df, FUN=mean)
+  
+  return(newdf)
+
+}
+
+# exponential fits -----
+
+exponentialModel <- function(par, timepoints, mode='learning', setN0=NULL) {
+  
+  if (length(timepoints) == 1) {
+    timepoints <- c(0:(timepoints-1))
+  }
+  
+  if (is.numeric(setN0)) {
+    par['N0'] = setN0
+  }
+  
+  if (mode == 'learning') {
+    output = par['N0'] - ( par['N0'] * (1-par['lambda'])^timepoints )
+  }
+  if (mode == 'washout') {
+    output = par['N0'] * (par['lambda'])^timepoints
+  }
+  
+  return(data.frame(trial=timepoints,
+                    output=output))
+  
+}
+
+exponentialMSE <- function(par, signal, timepoints=c(0:(length(signal)-1)), mode='learning', setN0=NULL) {
+  
+  MSE <- mean((exponentialModel(par, timepoints, mode=mode, setN0=setN0)$output - signal)^2, na.rm=TRUE)
+  
+  return( MSE )
+  
+}
+
+
+exponentialFit <- function(signal, timepoints=length(signal), mode='learning', gridpoints=11, gridfits=10, setN0=NULL) {
+  
+  # set the search grid:
+  parvals <- seq(1/gridpoints/2,1-(1/gridpoints/2),1/gridpoints)
+  
+  asymptoteRange <- c(-1,2)*max(abs(signal), na.rm=TRUE)
+  
+  # define the search grid:
+  # if (is.numeric(setN0)) {
+  #   searchgrid <- expand.grid('lambda' = parvals)
+  #   lo <- c(0)
+  #   hi <- c(1)
+  # }
+  if (is.null(setN0)) {
+    searchgrid <- expand.grid('lambda' = parvals,
+                              'N0'     = parvals * diff(asymptoteRange) + asymptoteRange[1] )
+    lo <- c(0,asymptoteRange[1])
+    hi <- c(1,asymptoteRange[2])
+  } else {
+    searchgrid <- expand.grid('lambda' = parvals )
+    lo <- c(0)
+    hi <- c(1)
+  }
+  # evaluate starting positions:
+  MSE <- apply(searchgrid, FUN=exponentialMSE, MARGIN=c(1), signal=signal, timepoints=timepoints, mode=mode, setN0=setN0)
+  
+  if (is.null(setN0)) {
+    X <- data.frame(searchgrid[order(MSE)[1:gridfits],])
+  } else {
+    X <- data.frame('lambda'=searchgrid[order(MSE)[1:3],])
+  }
+  
+  # run optimx on the best starting positions:
+  allfits <- do.call("rbind",
+                     apply( X,
+                            MARGIN=c(1),
+                            FUN=optimx::optimx,
+                            fn=exponentialMSE,
+                            method     = 'L-BFGS-B',
+                            lower      = lo,
+                            upper      = hi,
+                            timepoints = timepoints,
+                            signal     = signal,
+                            mode       = mode,
+                            setN0      = setN0 ) )
+  
+  # pick the best fit:
+  win <- allfits[order(allfits$value)[1],]
+  
+  if (is.null(setN0)) {
+    winpar <- unlist(win[1:2])
+  } else {
+    winpar <- c( 'lambda' = unlist(win[1]), 
+                 'N0'     = setN0)
+    names(winpar) <- c('lambda', 'N0')
+  }
+  
+  # return the best parameters:
+  return(winpar)
   
 }
