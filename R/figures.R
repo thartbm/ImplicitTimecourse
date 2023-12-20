e10ea2b87663783379f96895c2bc0ae2175b8f32
Marius 't Hart <thartbm@gmail.com>
2023-12-20 09:10:02 -0500

    add aiming timecourse plot to figure for exp 3, add function to make experiment-wise implicit/explicit scatters + linear models + marginal density plots

--- R/figures.R
+++ R/figures.R
@@ -5,44 +5,55 @@ source('R/utilities.R')
 # figure 1: setup, manipulations, trialtypes
 
 
-expFig <- function(exp, target='inline') {
+expBehaviorFig <- function(exp, target='inline') {
   
   # in inches:
-  width=7
-  height=7
+  width  =  7
+  height = 10
+  dpi = 300
+  outfilename <- sprintf('doc/fig%d',exp+1)
   
   if (target == 'svg') {
-    svglite::svglite( filename = sprintf('doc/fig%d.svg',exp+1),
+    svglite::svglite( filename = sprintf('%s.svg',outfilename),
                       width = width,
                       height = height,
                       fix_text_size = FALSE)
   }
   # if (target == 'png') {
-  #   
+  #   png( filename = sprintf('%s.png',outfilename),
+  #        width = width*dpi,
+  #        height = height*dpi
+  #        )
   # }
-  # if (target == 'pdf') {
-  #   
-  # }
+  if (target == 'pdf') {
+    pdf( file = sprintf('%s.pdf', outfilename),
+         width=width,
+         height=height)
+  }
   # if (target == 'tiff') {
-  #   
+  #   tiff( filename = sprintf('%s.tiff',outfilename),
+  #         compression = 'lzw',
+  #         width = width*dpi,
+  #         height = height*dpi
+  #   )
   # }
   
   
   info <- groupInfo()
+  conditions <- expConditions(exp)
   
-  if (exp == 1) { conditions <- c('15deg_distance', '30deg_distance', '45deg_distance', '60deg_distance') }
-  if (exp == 2) { conditions <- c('control','cursorjump','terminal') }
-  if (exp == 3) { conditions <- c('control','aiming') }
-  if (exp == 4) { conditions <- c('control','terminal','delay-trial', 'delay-FB')}
   
   if (exp > 1) { ntrials = 120; mrot=45; xticks=c(1,21,120) } else { ntrials = 144; mrot=60; xticks=c(1,21,121,144) }
   
-  layout( mat=matrix(c(1,2),nrow=2, byrow=TRUE) )
+  layout( mat=matrix(c(1,1,1,2,2,2,3,4,5),nrow=3,ncol=3, byrow=TRUE) )
   
   relfontsize <- 0.8
   
   par(mar=c(4,4,4,0.1),
       cex.axis=relfontsize, cex.lab=relfontsize, cex.main=relfontsize)
+  
+  
+  # # # # # #   First plot: reaches
   
   plot(x=-1000, y=-1000,
        main='training reaches',xlab='trial',ylab='deviation [°]',
@@ -74,8 +85,15 @@ expFig <- function(exp, target='inline') {
   axis(side=1,at=xticks)
   axis(side=2,at=seq(-15,(mrot+15),15),las=1)
   
+  # # # # # #   Second plot: no-cursors
+  
+  main <- 'no-cursor reaches'
+  if (exp == 3) {
+    main <- 'no-cursor reaches and aiming'
+  }
+  
   plot(x=-1000, y=-1000,
-       main='no-cursor reaches',xlab='trial',ylab='deviation [°]',
+       main=main,xlab='trial',ylab='deviation [°]',
        xlim=c(0,ntrials+1),ylim=c(-15,mrot+15),
        ax=F,bty='n')
   
@@ -93,8 +111,12 @@ expFig <- function(exp, target='inline') {
   addAimingTrials(mrot=mrot)
   
   leg.info <- addLearningCurves(type='nocursors',
-                                conditions=conditions)
+                                conditions=conditions
+                                )
   
+  if (exp == 3) {
+    leg.info <- rbind(leg.info, addLearningCurves(type='aiming',conditions=c('aiming'),FUN=mean))
+  }
   # legend(x=-5,y=mrot+20,
   #        legend=leg.info$label,
   #        col=leg.info$color,
@@ -102,6 +124,93 @@ expFig <- function(exp, target='inline') {
   
   axis(side=1,at=xticks)
   axis(side=2,at=seq(-15,(mrot+15),15),las=1)
+  
+  
+  
+  
+  
+  if (target %in% c('svg','png','pdf','tiff')) {
+    dev.off()
+  }
+  
+}
+
+
+implExplFig <- function(exp, target='inline') {
+  
+  # in inches:
+  width  =  4
+  height = 4
+  dpi = 300
+  outfilename <- sprintf('doc/fig%d',exp+5)
+  
+  if (target == 'svg') {
+    svglite::svglite( filename = sprintf('%s.svg',outfilename),
+                      width = width,
+                      height = height,
+                      fix_text_size = FALSE)
+  }
+  # if (target == 'png') {
+  #   png( filename = sprintf('%s.png',outfilename),
+  #        width = width*dpi,
+  #        height = height*dpi
+  #        )
+  # }
+  if (target == 'pdf') {
+    pdf( file = sprintf('%s.pdf', outfilename),
+         width=width,
+         height=height)
+  }
+  # if (target == 'tiff') {
+  #   tiff( filename = sprintf('%s.tiff',outfilename),
+  #         compression = 'lzw',
+  #         width = width*dpi,
+  #         height = height*dpi
+  #   )
+  # }
+  
+  
+  
+  info <- groupInfo()
+  conditions <- expConditions(exp)
+  
+  if (exp > 1) { mrot=45 } else { mrot=60 }
+  
+  
+  layout( mat=matrix(c(2,3,4,1),nrow=2,ncol=2, byrow=TRUE), widths = c(1,3), heights = c(3,1) )
+  
+  relfontsize <- 0.8
+  
+  par(mar=c(4,4,0.1,0.1),
+      cex.axis=relfontsize, cex.lab=relfontsize, cex.main=relfontsize)
+  
+  implicit <- list()
+  explicit <- list()
+  
+  plot(x=-1000, y=-1000,
+       main='',xlab='explicit (aiming) [°]',ylab='',
+       ylim=c(0,1),xlim=c(-15,mrot+15),
+       ax=F,bty='n')
+  
+  addDensities(conditions, type='aiming', viewscale=c(1,-1), flipXY=FALSE)
+
+  plot(x=-1000, y=-1000,
+       main='',ylab='implicit (no-cursor) [°]',xlab='',
+       xlim=c(0,1),ylim=c(-15,mrot+15),
+       ax=F,bty='n')
+  
+  addDensities(conditions, type='nocursors', flipXY=TRUE, viewscale=c(-1,1))
+  
+  plot(x=-1000, y=-1000,
+       main='',ylab='',xlab='',
+       xlim=c(-15,mrot+15),ylim=c(-15,mrot+15),
+       ax=F,bty='n')
+  
+  addImpExpScatters(conditions)
+  
+  axis(side=1,at=seq(-15,mrot+15,15))
+  axis(side=2,at=seq(-15,mrot+15,15))
+  
   
   if (target %in% c('svg','png','pdf','tiff')) {
     dev.off()
