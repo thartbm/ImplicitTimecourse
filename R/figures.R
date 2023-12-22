dc0b936e7d27979f1e1b2942a895070025a47cc2
Marius 't Hart <thartbm@gmail.com>
2023-12-22 08:32:59 -0500

    added exponential fit timecourses and explicit-implicit scatters to figures

--- R/figures.R
+++ R/figures.R
@@ -8,8 +8,8 @@ source('R/utilities.R')
 expBehaviorFig <- function(exp, target='inline') {
   
   # in inches:
-  width  =  7
-  height = 10
+  width = 7
+  if (exp==1) { height = 10 } else { height = 7.5 }
   dpi = 300
   outfilename <- sprintf('doc/fig%d',exp+1)
   
@@ -45,7 +45,11 @@ expBehaviorFig <- function(exp, target='inline') {
   
   if (exp > 1) { ntrials = 120; mrot=45; xticks=c(1,21,120) } else { ntrials = 144; mrot=60; xticks=c(1,21,121,144) }
   
-  layout( mat=matrix(c(1,1,1,2,2,2,3,4,5),nrow=3,ncol=3, byrow=TRUE) )
+  if (exp == 1) {
+    layout( mat=matrix(c(1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,4,4,4,5,5,6,6,7,7),nrow=4,ncol=6, byrow=TRUE) )
+  } else {
+    layout( mat=matrix(c(1,1,1,2,2,2,3,4,5),nrow=3,ncol=3, byrow=TRUE) )
+  }
   
   relfontsize <- 0.8
   
@@ -76,8 +80,10 @@ expBehaviorFig <- function(exp, target='inline') {
   leg.info <- addLearningCurves(type='reaches',
                                 conditions=conditions)
   
+  legends <- convertLegend(leg.info$label)
+  
   legend(x=c(-5,120)[(exp==1)+1],y=mrot+20,
-         legend=leg.info$label,
+         legend=legends,
          col=leg.info$color,
          lty=1, bty='n',
          cex=relfontsize)
@@ -125,7 +131,79 @@ expBehaviorFig <- function(exp, target='inline') {
   axis(side=1,at=xticks)
   axis(side=2,at=seq(-15,(mrot+15),15),las=1)
   
+  plot(-1000,-1000,
+       main='learning timecourse',xlab='trial',ylab='adaptation',
+       xlim=c(0,30),ylim=c(0,1),
+       ax=F,bty='n'
+       )
   
+  addAdaptationTimecourses(type='reaches',conditions=conditions)
+  
+  axis(side=1,at=c(1,10,20,30))
+  axis(side=2,at=c(0,0.25,0.5,0.75,1),las=1,labels=c('0%','25%','50%','75%','100%'))
+  
+  
+  plot(-1000,-1000,
+       main='no-cursor timecourse',xlab='trial',ylab='adaptation',
+       xlim=c(0,30),ylim=c(0,1),
+       ax=F,bty='n'
+  )
+  
+  addAdaptationTimecourses(type='nocursors',conditions=conditions)
+  
+  if (exp == 3) {
+    addAdaptationTimecourses(type='aiming',conditions=c('aiming'))
+  }
+  
+
+  axis(side=1,at=c(0,10,20,30),labels=c('baseline','10','20','30'))
+  axis(side=2,at=c(0,0.25,0.5,0.75,1),las=1,labels=c('0%','25%','50%','75%','100%'))
+  
+  
+  if (exp == 1) {
+    plot(-1000,-1000,
+         main='washout reach timecourses',xlab='trial',ylab='reach aftereffects',
+         xlim=c(0,30),ylim=c(0,45),
+         ax=F,bty='n'
+    )
+    
+    addWashoutTimecourses(type='reaches',conditions=conditions)
+    
+    axis(side=1,at=c(1,12,24))
+    axis(side=2,at=c(0,15,30,45),las=1)
+    
+    plot(-1000,-1000,
+         main='washout no-cursor timecourses',xlab='trial',ylab='no-cursor aftereffects',
+         xlim=c(0,30),ylim=c(0,45),
+         ax=F,bty='n'
+    )
+    
+    addWashoutTimecourses(type='nocursors',conditions=conditions)
+    
+    axis(side=1,at=c(1,12,24))
+    axis(side=2,at=c(0,15,30,45),las=1)
+    
+  }
+  
+  
+  # now the implicit/explicit scatters:
+  
+  if (exp > 1) { mrot=45 } else { mrot=60 }
+  mrot <- 60
+  
+  plot(x=-1000, y=-1000,
+       main='',ylab='implicit (no-cursors) [°]',xlab='explicit (aiming) [°]',
+       xlim=c(-15,mrot+15),ylim=c(-15,mrot+15),
+       ax=F,bty='n')
+  
+  addImpExpScatters(conditions)
+  
+  addDensities(conditions, type='aiming', viewscale=c(1,12.5), offset=c(0,mrot+2.5))
+  
+  addDensities(conditions, type='nocursors', viewscale=c(12.5,1), flipXY=TRUE, offset=c(mrot+2.5,0))
+  
+  axis(side=1,at=seq(0,mrot,15))
+  axis(side=2,at=seq(0,mrot,15))
   
   
   
@@ -184,8 +262,8 @@ implExplFig <- function(exp, target='inline') {
   par(mar=c(4,4,0.1,0.1),
       cex.axis=relfontsize, cex.lab=relfontsize, cex.main=relfontsize)
   
-  implicit <- list()
-  explicit <- list()
+  # implicit <- list()
+  # explicit <- list()
   
   plot(x=-1000, y=-1000,
        main='',xlab='explicit (aiming) [°]',ylab='',
