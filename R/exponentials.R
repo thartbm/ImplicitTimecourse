0741c08fbc68d0175a312df0591429f47f741c73
Marius 't Hart <thartbm@gmail.com>
2023-12-22 20:03:58 -0500

    corrected minor bugs, added function to get individual fits

--- R/exponentials.R
+++ R/exponentials.R
@@ -2,13 +2,6 @@ library('parallel')
 source('R/utilities.R')
 # advanced pre-processing -----
 
-allExponentialBootstraps <- function() {
-  
-  bootstrapAllLearningExpFits()
-  bootstrapAllAimingExpFits()
-  bootstrapAllWashoutExpFits()
-  
-}
 
 bootstrapExponentialLearning <- function(condition, type, iterations=1000, clust=NULL, depvar='reachdeviation_deg') {
   
@@ -23,7 +16,7 @@ bootstrapExponentialLearning <- function(condition, type, iterations=1000, clust
   df <- baseline(df, depvar=depvar)
 
   df$depvar <- df[,depvar]
-  if (depvar %in% c('reachdeviation_deg', 'aiming')) {
+  if (depvar %in% c('reachdeviation_deg', 'aimingdeviation_deg')) {
     df$depvar <- -1 * df$depvar
   }
   df <- df[which(df$phase == 'rotation' & df$trialno < 77),]
@@ -77,11 +70,29 @@ fitExp <- function(participants, df, prepend=c(), mode='learning') {
       dfs <- pdf
     }
   }
-    
+  
+  # if (length(participants) > 1) {
+  #   id <- 'all'
+  # } else {
+  #   id <- participants[1]
+  # }
+  
   agdf <- aggregate(depvar ~ trialno, data=dfs, FUN=mean, na.rm=TRUE)
   
   signal <- c(prepend, agdf$depvar)
   
+  # if (savetempfig) {
+  #   if ('type' %in% names(df)) {type=df$type[1]} else {type <- 'undefined'}
+  #   if ('condition' %in% names(df)) {condition=df$condition[1]} else {condition <- 'undefined'}
+  #   
+  #   dir.create(sprintf('temp/%s/',condition), showWarnings = FALSE)
+  #   dir.create(sprintf('temp/%s/%s/',condition, type), showWarnings = FALSE)
+  #   
+  #   pdf(file=sprintf('temp/%s/%s/%s_%s.pdf',condition,type,id,mode))
+  #   plot(signal,type='l')
+  #   dev.off()
+  # }
+
   # if (mode=='washout') {
   #   setN0 <- signal[1]
   #   gridpoints=5
@@ -96,7 +107,7 @@ fitExp <- function(participants, df, prepend=c(), mode='learning') {
   
 }
 
-bootstrapAllLearningExpFits <- function(iterations=5000) {
+bootstrapAllLearningExpFits <- function(iterations=5000, conditions=NULL) {
   
   ncores   <- parallel::detectCores()
   clust <- parallel::makeCluster(max(c(1,floor(ncores*0.75))))
@@ -105,7 +116,11 @@ bootstrapAllLearningExpFits <- function(iterations=5000) {
   
   types <- c('reaches', 'nocursors')
   
-  for (condition in info$condition) {
+  if (is.null(conditions)) {
+    conditions <- info$condition
+  }
+  
+  for (condition in conditions) {
     for (type in types) {
       cat(sprintf('exponential fit (%d iterations) for: %s %s\n',iterations,condition,type))
       bootstrapExponentialLearning(condition, type, iterations=iterations, clust=clust)
@@ -231,3 +246,127 @@ bootstrapExponentialWashout <- function(condition, type, iterations=5000, clust=
 }
 
 
+# group average and individual fits -----
+
+groupAvgFits <- function(conditions=NULL) {
+  
+  # first make a cluster:
+  ncores <- parallel::detectCores()
+  clust  <- parallel::makeCluster(max(c(1,floor(ncores*0.75))))
+  
+  # get some meta info:
+  info       <- groupInfo()
+  
+  if (is.null(conditions)) { conditions <- info$condition }
+  
+  for (condition in conditions) {
+    print(condition)
+    condition_idx <- which(info$condition == condition)
+    
+    exp <- info$exp[condition_idx]
+    conditiontypes <- c('reaches', 'nocursors')
+    if (condition == 'aiming') {conditiontypes <- c(conditiontypes, 'aiming')}
+    
+    # outputs stored here:
+    cdf <- NA
+    
+    for (type in conditiontypes) {
+      df <- read.csv(sprintf('data/exp%d/%s_%s.csv',exp,condition,type))
+      
+      depvar <- list('reaches'='reachdeviation_deg',
+                     'nocursors'='reachdeviation_deg',
+                     'aiming'='aimingdeviation_deg')[[type]]
+      
+      prepend <- c()
+      if (type == 'nocursors') {prepend <- c(0)}
+      
+      df <- removeOutliers(df, depvar=depvar)
+      df <- baseline(df, depvar=depvar)
+      
+      df$depvar <- -1 * df[,depvar]
+      df$type <- type
+      
+      # first the learning/adaptation timecourses
+      ldf <- df[which(df$phase == 'rotation' & df$trialno < 77),]
+      
+      pps <- list()
+      pps[['all']] <- unique(ldf$participant)
+      for (pp in unique(ldf$participant)) {
+        pps[[pp]] <- pp
+      }
+      
+      # print(pps)
+      
+      a <- parLapply(cl = clust,
+                    X = pps,
+                    # MARGIN = 1,
+                    fun = fitExp,
+                    df = ldf,
+                    prepend = prepend,
+                    mode = 'learning')
+      
+      # print(a)
+      
+      participant <- c()
+      lambda <- c()
+      N0 <- c()
+      for (idx in names(a)) {
+        participant <- c(participant, idx)
+        lambda <- c(lambda, a[[idx]]['lambda'])
+        N0 <- c(N0, a[[idx]]['N0'])
+      }
+      
+      adf <- data.frame(participant, lambda, N0)
+      adf$phase <- 'learning'
+      adf$trialtype <- type
+      
+      if (is.data.frame(cdf)) {
+        cdf <- rbind(cdf, adf)
+      } else {
+        cdf <- adf
+      }
+      
+      if (exp == 1) {
+        
+        # also do washout
+        wdf <- getWashout(df)
+
+        # no baseline (or steady state / asymptote) appended for no-cursors
+        prepend <- c()
+        
+        a <- parLapply(cl = clust,
+                       X = pps,
+                       fun = fitExp,
+                       df = wdf,
+                       prepend = prepend,
+                       mode = 'washout')
+        
+        participant <- c()
+        lambda <- c()
+        N0 <- c()
+        for (idx in names(a)) {
+          participant <- c(participant, idx)
+          lambda <- c(lambda, a[[idx]]['lambda'])
+          N0 <- c(N0, a[[idx]]['N0'])
+        }
+        
+        adf <- data.frame(participant, lambda, N0)
+        adf$phase <- 'washout'
+        adf$trialtype <- type
+        
+        cdf <- rbind(cdf, adf)
+        
+      }
+      
+    }
+    
+    write.csv(x         = cdf, 
+              file      = sprintf('data/exp%d/%s_individual_exp-fits.csv',exp,condition), 
+              row.names = FALSE,
+              quote     = TRUE)
+    
+  }
+  
+  stopCluster(clust)
+  
+} 
